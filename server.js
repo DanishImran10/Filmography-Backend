fa769b2bf12ea541d65cd2431301a6c132a2b8ba
Danish Imran <danishimran015@gmail.com>
2026-09-10 22:05:25 +0500

    server setup

---
+++ server.js
@@ -0,0 +1,23 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+
+dotenv.config({
+   path: "./.env" 
+});
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(cors());
+
+app.get("/", (req, res) => {
+    res.status(200).json({
+        message: "hoolla",
+        ind: 3
+    });
+});
+
+app.listen(PORT, () => {
+    console.log(`Server listening on Port ${PORT}`);
+});
